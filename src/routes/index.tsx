2617dabbab0d55c6c482abe5e0ecd7163c679b9f
naoya <yamagata_7580@yahoo.co.jp>
2026-04-19 22:04:55 +0900

    各種data fetchの際のコンポーネントの確認()

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,48 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, notFound, useRouter } from "@tanstack/react-router";
 import SkillCard from "#/components/SkillCard.tsx";
 
-export const Route = createFileRoute("/")({ component: App });
+const POKE_API_URL = "https://pokeapi.co/api/v2/pokemon";
+
+export const Route = createFileRoute("/")({
+	component: App,
+	pendingComponent: () => <div className="p-14 text-center">Loading...</div>,
+	pendingMs: 300,
+	loader: async () => {
+		const response = await fetch(POKE_API_URL);
+		const data = await response.json();
+
+		if(data.results.length === 0 || !data.results) {
+			throw notFound();
+		}
+
+		return data;
+	},
+	errorComponent: ({ error }) => {
+		const router = useRouter();
+
+		return (
+			<div className="p-14">
+				<p>エラーが発生しました: {error.message}</p>
+				<button className="cursor-pointer" type="button" onClick={() => router.invalidate()}>
+					エラーをクリアして再読み込み
+				</button>
+			</div>
+		);
+	},
+	notFoundComponent: () => <div className="p-14 text-center">Not Found</div>,
+});
 
 function App() {
+	const data = Route.useLoaderData();
 	return (
 		<main className="page-wrap px-4 pb-8 pt-14">
 			<h1>Hello World</h1>
 			<ul className="mt-6 list-none p-0 space-y-5">
-				<li>
-					<SkillCard name="Tanstack Start" />
-				</li>
-				<li>
-					<SkillCard name="Typescript" />
-				</li>
-				<li>
-					<SkillCard name="React" />
-				</li>
-				<li>
-					<SkillCard name="SQL" />
-				</li>
+				{data.results.map((pokemon: { name: string }) => (
+					<li key={pokemon.name}>
+						<SkillCard name={pokemon.name} />
+					</li>
+				))}
 			</ul>
 		</main>
 	);
